1e8925179ead14b48522438635c4c0a19f7c14db
Mohammad Obiedat <mohammad.o@tradervolt.com>
2025-05-16 15:43:16 +0300

    .

--- KeyCloak.Infrastructure/Identity/CredentialRepresentation.cs
+++ KeyCloak.Infrastructure/Identity/CredentialRepresentation.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KeyCloak.Infrastructure.Identity;
+﻿namespace KeyCloak.Infrastructure.Identity;
 
 public sealed record CredentialRepresentation(string Type, string Value, bool Temporary);
 
